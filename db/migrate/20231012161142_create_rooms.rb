d20ab104f2387238bcde867b914e807582e572bf
Kaina Cockett <kainacockett@gmail.com>
2023-10-12 11:17:14 -0600

    fix: update db for 3 hints per puzzle

--- db/migrate/20231012161142_create_rooms.rb
+++ db/migrate/20231012161142_create_rooms.rb
@@ -1,16 +1,27 @@
 class CreateRooms < ActiveRecord::Migration[7.0]
   def change
     create_table :rooms do |t|
-      t.text :name
+      t.text :room_name
       t.time :fastest_time
       t.text :fastest_time_nickname
-      t.integer :allowed_hints
-      t.text :puzzle_1_hint
-      t.text :puzzle_2_hint
-      t.text :puzzle_3_hint
-      t.text :puzzle_4_hint
-      t.text :puzzle_5_hint
-      t.text :puzzle_6_hint
+      t.text :puzzle_1_hint_1
+      t.text :puzzle_1_hint_2
+      t.text :puzzle_1_hint_3
+      t.text :puzzle_2_hint_1
+      t.text :puzzle_2_hint_2
+      t.text :puzzle_2_hint_3
+      t.text :puzzle_3_hint_1
+      t.text :puzzle_3_hint_2
+      t.text :puzzle_3_hint_3
+      t.text :puzzle_4_hint_1
+      t.text :puzzle_4_hint_2
+      t.text :puzzle_4_hint_3
+      t.text :puzzle_5_hint_1
+      t.text :puzzle_5_hint_2
+      t.text :puzzle_5_hint_3
+      t.text :puzzle_6_hint_1
+      t.text :puzzle_6_hint_2
+      t.text :puzzle_6_hint_3
       t.timestamps
     end
   end
