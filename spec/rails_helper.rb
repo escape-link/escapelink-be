d1bed0b2ddbb83788e5d5f4d31c62ee28947a9e4
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-10-15 15:39:50 -0400

    Chore: adds factory_bot config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,4 @@
-require "simplecov"
+require 'simplecov'
 SimpleCov.start
 require 'faker'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
@@ -6,7 +6,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -33,7 +33,6 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -64,6 +63,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
 
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
