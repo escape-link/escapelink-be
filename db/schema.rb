a63c3af5e23ee3b3acbaa892477bf372b8459df5
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-10-15 15:25:04 -0400

    Feat: updates DB for messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_14_184911) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_15_185355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "channels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.integer "puzzle_1_solved", default: 0
@@ -25,4 +31,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_184911) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.bigint "channel_id", null: false
+    t.string "content"
+    t.string "sender_nickname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel_id"], name: "index_messages_on_channel_id"
+  end
+
+  add_foreign_key "messages", "channels"
 end
