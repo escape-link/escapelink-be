8ddb8377094a960137eabca520f129632abf9d9a
Kaina Cockett <kainacockett@gmail.com>
2023-10-16 11:51:03 -0600

    test: update leaderboard tests with new col names

--- spec/models/leaderboard_spec.rb
+++ spec/models/leaderboard_spec.rb
@@ -12,29 +12,29 @@ RSpec.describe Leaderboard, type: :model do
       time_seconds = 5400
       team_entry = Leaderboard.create!(group_name: name, time_seconds: time_seconds)
 
-      expect(team_entry.name).to be_a(String)
-      expect(team_entry.name).to eq(name)
-      expect(team_entry.score).to eq(time_seconds)
+      expect(team_entry.group_name).to be_a(String)
+      expect(team_entry.group_name).to eq(name)
+      expect(team_entry.time_seconds).to eq(time_seconds)
     end
     
     it "SAD PATH:  should not initialize with missing/nil values" do
       # name = "The Husslers"
       time_seconds = 5400
-      team_entry = Leaderboard.create!(group_name: name, time_seconds: time_seconds)
+      team_entry = Leaderboard.create!(time_seconds: time_seconds)
 
-      expect(team_entry.name).to_not be_a(String)
-      expect(team_entry.name).to_not eq(name)
-      expect(team_entry.score).to_not eq(time_seconds)
+      expect(team_entry.group_name).to_not be_a(String)
+      expect(team_entry.group_name).to_not eq(name)
+      expect(team_entry.time_seconds).to_not eq(time_seconds)
     end
     
     it "SAD PATH:  should not initialize with missing values" do
       name = "The Husslers"
       # time_seconds = 5400
-      team_entry = Leaderboard.create!(group_name: name, time_seconds: time_seconds)
+      team_entry = Leaderboard.create!(group_name: name)
 
-      expect(team_entry.name).to_not be_a(String)
-      expect(team_entry.name).to_not eq(name)
-      expect(team_entry.score).to_not eq(time_seconds)
+      expect(team_entry.group_name).to_not be_a(String)
+      expect(team_entry.group_name).to_not eq(name)
+      expect(team_entry.time_seconds).to_not eq(time_seconds)
     end
   end
 end
