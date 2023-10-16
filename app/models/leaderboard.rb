f72c04162364edb3c26e96bc2ff3e4c68b7695d8
Dani Wilson <danirae.powell@gmail.com>
2023-10-16 13:44:51 -0600

    feat: begin pseudocoding leaderboard method

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -1,4 +1,15 @@
 class Leaderboard < ApplicationRecord
   validates :group_name, presence: true
   validates :time_seconds, presence: true
+
+
+  def self.update_leaderboard(group_name, time_seconds)
+    #check existing leaderboard scores
+    #if the new score is less than the other ten, 
+    #drop the row with highest time and add in the new
+    # leaderboard_scores.each do |leaderboard_score|
+      # if time_seconds < leaderboard_score
+      # leaderboard_score.last.destroy
+
+  end
 end
