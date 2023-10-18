ea1b5ae017ac3e1cf8959fba2a76a10a1ac709f4
Kaina Cockett <kainacockett@gmail.com>
2023-10-18 16:41:53 -0600

    fix: update leaderboard test for new schema

--- spec/models/leaderboard_spec.rb
+++ spec/models/leaderboard_spec.rb
@@ -9,21 +9,24 @@ RSpec.describe Leaderboard, type: :model do
 
   describe "#initialize" do
     it "HAPPY PATH:  should initialize with input values" do
-      name = "The Husslers"
-      time_seconds = 5400
-      room = Room.create!(room_name: "Where's Bob?", number_puzzles: 5)
-      team_entry = Leaderboard.create!(room_id: room.id, game_name: name, time_seconds: time_seconds)
+      leaderboard = create_list(:leaderboard, 10)
+      room_id = leaderboard.first.room_id
+      game = Game.create!(room_id: room_id)
+      finish_group_name = game.game_name
+      finish_score = 900 #fastest time in seconds
+
+      team_entry = Leaderboard.create!(room_id: room_id, game_name: game.game_name, time_seconds: finish_score)
 
       expect(team_entry.game_name).to be_a(String)
-      expect(team_entry.game_name).to eq(name)
-      expect(team_entry.time_seconds).to eq(time_seconds)
-      expect(team_entry.room_id).to eq(room.id)
+      expect(team_entry.game_name).to eq(finish_group_name)
+      expect(team_entry.time_seconds).to eq(finish_score)
+      expect(team_entry.room_id).to eq(room_id)
     end
     
     it "SAD PATH:  should not initialize with missing/nil values" do
       # name = "The Husslers"
       time_seconds = 5400
-      team_entry = Leaderboard.new(time_seconds: time_seconds)
+      team_entry = Leaderboard.new(room_id: nil, game_name: nil, time_seconds: time_seconds)
 
       expect(team_entry).to_not be_valid
       expect(team_entry.errors[:game_name]).to include("can't be blank")
@@ -41,23 +44,25 @@ RSpec.describe Leaderboard, type: :model do
 
   describe "update leaderboard" do
     it "HAPPY PATH: the new score fits in the top ten" do
-      scores = create_list(:leaderboard, 10)
-
-      new_game_name = "Ecstatic-Chartreuse-SonicTheHedgehog"
-      new_score = 850
-
-      message = Leaderboard.update_leaderboard(new_game_name, new_score)
+      leaderboard = create_list(:leaderboard, 10)
+      room_id = leaderboard.first.room_id
+      game = Game.create!(room_id: room_id)
+      finish_group_name = game.game_name
+      finish_score = 900 #fastest time in seconds
+      
+      message = Leaderboard.update_leaderboard(room_id: room_id, game_name: finish_group_name, time_seconds: finish_score)
 
       expect(message).to eq("Congratulations! You've claimed a spot on the leaderboard!")
     end
 
     it "SAD PATH: the new score does not fit into the top ten" do
-      scores = create_list(:leaderboard, 10)
-
-      new_game_name = "Frolicking-ArmyGreen-KnucklesTheEchidna"
-      new_score = 3100
-
-      message = Leaderboard.update_leaderboard(new_game_name, new_score)
+      leaderboard = create_list(:leaderboard, 10)
+      room_id = leaderboard.first.room_id
+      game = Game.create!(room_id: room_id)
+      finish_group_name = game.game_name
+      finish_score = 4000 #slowest time in seconds
+      
+      message = Leaderboard.update_leaderboard(room_id: room_id, game_name: finish_group_name, time_seconds: finish_score)
 
       expect(message).to eq("Sorry, you didn't make the leaderboard.")
     end
