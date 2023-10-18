5980e082d372a297311695eb40943c3a7a73f997
Dani Wilson <danirae.powell@gmail.com>
2023-10-17 20:03:41 -0600

    chore: add migration
    CO-AUTHORED-BY <kainacockett@gmail.com>

--- db/migrate/20231018004049_create_game_puzzles.rb
+++ db/migrate/20231018004049_create_game_puzzles.rb
@@ -1,7 +1,7 @@
 class CreateGamePuzzles < ActiveRecord::Migration[7.0]
   def change
     create_table :game_puzzles do |t|
-      t.integer :puzzle_solved
+      t.integer :puzzle_solved, default: 0
       t.references :game, null: false, foreign_key: true
       t.references :puzzle, null: false, foreign_key: true
 
