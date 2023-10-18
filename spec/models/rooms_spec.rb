1f51b78121888dcd4eb9b8bf9d80886a8e548497
Dani Wilson <danirae.powell@gmail.com>
2023-10-17 19:03:23 -0600

    test: add test files for model tests
    CO-AUTHORED-BY <kainacockett@gmail.com>

---
+++ spec/models/rooms_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe Room, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:room_name) }
+    it { should validate_presence_of(:number_puzzles) }
+  end
+
+  describe "#initialize" do
+    it "should initialize" do
+      room_name = "where's bob?"
+      number_puzzles = 5
+      room = Room.new(room_name: room_name, number_puzzles: number_puzzles)
+
+      expect(room).to be_valid
+      expect(room.room_name).to eq(room_name)
+      expect(room.number_puzzles).to eq(number_puzzles)
+    end
+
+    it "should not initialize with missing information" do
+      number_puzzles = 5
+      room = Room.new(number_puzzles: number_puzzles)
+
+      expect(room).to_not be_valid
+
+      room_name = "where's bob?"
+      room = Room.new(room_name: room_name)
+
+      expect(room).to_not be_valid
+    end
+  end
+end
