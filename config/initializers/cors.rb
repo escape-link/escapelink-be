1f2c8bb18533d7253981f1ad91a4efb965da017d
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-10-19 12:51:03 -0400

    Chore: removes trailing slash

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://escapelink-fe.vercel.app/' # Adjust this as per your front-end server's address and port
+    origins 'https://escapelink-fe.vercel.app' # Adjust this as per your front-end server's address and port
 
     resource '*',
              headers: :any,
