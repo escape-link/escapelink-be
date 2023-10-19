675073dbaabcf09791e09a61416339c1e31ea31b
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-10-19 18:50:52 -0400

    Feat: updates game controller to work with room_id from front end

--- app/controllers/api/v0/games_controller.rb
+++ app/controllers/api/v0/games_controller.rb
@@ -1,21 +1,22 @@
 class Api::V0::GamesController < ApplicationController
   def create
-    room = Room.find_by(room_name: game_params["room_name"])
-    game = Game.create!(room_id: room.id)
-    ActionCable.server.remote_connections.where(channel: game.game_name).disconnect
-    game_link = api_v0_games_path(game_name: game.game_name)
+    room_id = params[:room_id]
+
+    unless room_id.present? && room_id.to_i > 0
+      render json: { error: 'Invalid room_id' }, status: :unprocessable_entity
+      return
+    end
+
+    game = Game.create!(room_id:)
+    channel_name = "room_#{room_id}"
+    ActionCable.server.remote_connections.where(channel: channel_name).disconnect
+    game_link = api_v0_games_path(game_name: channel_name)
 
     response = {
       game_name: game.game_name,
-      game_link: game_link
+      game_link:
     }
 
     render json: response, status: :created
-  end
-
-  private
-
-  def game_params
-    params.permit("room_name")
   end
 end
