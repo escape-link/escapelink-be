dfeac54b7827491444bdbac76e74e1c95906aa8f
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-10-19 11:21:36 -0400

    Chore: adds FE deployment credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  config.action_cable.allowed_request_origins = ['http://localhost:3001']
+  config.action_cable.allowed_request_origins = ['https://escapelink-fe.vercel.app/']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
