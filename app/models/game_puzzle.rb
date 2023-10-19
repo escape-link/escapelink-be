ea3ad17fa6fce6314011df36a4937aa68a8a0547
Dani Wilson <danirae.powell@gmail.com>
2023-10-19 15:31:33 -0600

    feat: add update puzzle method
    CO-AUTHORED-BY: <kainacockett@gmail.com>

--- app/models/game_puzzle.rb
+++ app/models/game_puzzle.rb
@@ -3,4 +3,13 @@ class GamePuzzle < ApplicationRecord
 
   belongs_to :puzzle
   belongs_to :game
+
+  def update_puzzle(room_name:, puzzle_identifier:, game_name:)
+    game = Game.find_by(game_name: game_name)
+    room = Room.find_by(room_name: room_name)
+    puzzle = Puzzle.find_by(puzzle_identifier: puzzle_identifier, room_id: room.id)
+    game_puzzle = GamePuzzle.find_by(puzzle_id: puzzle.id, game_id: game.id)
+    
+    game_puzzle.update(puzzle_solved: 1)
+  end
 end
