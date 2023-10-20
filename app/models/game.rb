e48938a2f1c6e63c667972fb2ff25c03378788ac
Dani Wilson <danirae.powell@gmail.com>
2023-10-20 14:08:17 -0600

    add private method, refactor model methods
    CO-AUTHORED-BY: <kainacockett@gmail.com>

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
   before_validation :generate_game_name, if: :new_record? 
+  after_create :start_timer
   
   validates_presence_of :game_name, :room_id
 
@@ -8,36 +9,35 @@ class Game < ApplicationRecord
 
   def end_game(room_id:, game_name:)
     game = Game.find_by(game_name: game_name)
-    game.stop_timer(game_name: game_name)
+    time_seconds = game.stop_timer(game_name: game_name)
     tear_down_game(room_id: room_id, game_name:game_name)
-    leaderboard_message = Leaderboard.update_leaderboard(room_id: room_id, game_name: game_name, time_seconds: @time_seconds)
+    leaderboard_message = Leaderboard.update_leaderboard(room_id: room_id, game_name: game_name, time_seconds: time_seconds)
   end
 
-  def start_timer(game_name:)
+  def stop_timer(game_name:)
     game = Game.find_by(game_name: game_name)
-    game.update(start_time: Time.now)
+    end_time = DateTime.now.utc
+    start_time = game.start_time
+    time_seconds = (end_time - start_time)
   end
-
+  
   private
-
+  
   require 'faker'
-
+  
   def generate_game_name
     a = Faker::Adjective.negative
     b = Faker::Color.color_name
     c = Faker::Games::Pokemon.name
     self.game_name = "#{a}-#{b}-#{c}"
   end
-
+  
   def tear_down_game(room_id:, game_name:)
     game_to_remove = Game.where(room_id: room_id, game_name: game_name).first
     game_to_remove&.destroy
   end
-
-  def stop_timer(game_name:)
-    game = Game.find_by(game_name: game_name)
-    end_time = Time.now
-    start_time = game.start_time
-    @time_seconds = (end_time - start_time).to_i
+  
+  def start_timer
+    self.update(start_time: Time.now)
   end
 end
