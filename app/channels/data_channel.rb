f62cf6ef739466a9415c34a805ba17c0850e91b8
Dani Wilson <danirae.powell@gmail.com>
2023-10-21 16:01:46 -0600

    feat: add receive method for datachannel
    CO-AUTHORED-BY: <kainacockett@gmail.com>

--- app/channels/data_channel.rb
+++ app/channels/data_channel.rb
@@ -4,7 +4,14 @@ class DataChannel < ApplicationCable::Channel
   end
 
   def receive(data)
+    game_name = params[:game_name]
+    puzzle_identifier = data["puzzle_identifier"]
+    game = Game.find_by(game_name: game_name)
+    room_name = Room.find(game.room_id).room_name
+    game_status = GamePuzzle.update_puzzle(room_name: room_name, puzzle_identifier: puzzle_identifier, game_name: game_name)
     ActionCable.server.broadcast("data_#{params[:game_name]}", data)
+    ActionCable.server.broadcast("data_#{params[:game_name]}", game_status)
+
   end
 
   def unsubscribed
