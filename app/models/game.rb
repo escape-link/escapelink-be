b5843e4c9bee77035c0e8dcde807b849b1706654
Dani Wilson <danirae.powell@gmail.com>
2023-10-21 14:26:00 -0600

    add stop timer, start timer, refactor end game methods
    CO-AUTHORED-BY: <kainacockett@gmail.com>

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,15 +1,25 @@
 class Game < ApplicationRecord
-  before_validation :generate_game_name
-  after_create :generate_game_puzzles
+  before_validation :generate_game_name, if: :new_record? 
+  after_create :generate_game_puzzles 
+  after_create :start_timer
   
   validates_presence_of :game_name, :room_id
 
   has_many :game_puzzles
   has_many :puzzles, through: :game_puzzles
 
-  def end_game(room_id:, game_name:, time_seconds:)
+  def end_game(room_id:, game_name:)
+    game = Game.find_by(game_name: game_name)
+    time_seconds = game.stop_timer(game_name: game_name)
     tear_down_game(room_id: room_id, game_name:game_name)
     leaderboard_message = Leaderboard.update_leaderboard(room_id: room_id, game_name: game_name, time_seconds: time_seconds)
+  end
+
+  def stop_timer(game_name:)
+    game = Game.find_by(game_name: game_name)
+    end_time = DateTime.now.utc
+    start_time = game.start_time
+    time_seconds = (end_time - start_time)
   end
   
   private
@@ -26,6 +36,10 @@ class Game < ApplicationRecord
   def tear_down_game(room_id:, game_name:)
     game_to_remove = Game.where(room_id: room_id, game_name: game_name).first
     game_to_remove&.destroy
+  end
+
+  def start_timer
+    self.update(start_time: Time.now)
   end
 
   def generate_game_puzzles
