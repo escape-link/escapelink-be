5c5684955eee1d38320694586ef60c27fd83db9a
Dani Wilson <danirae.powell@gmail.com>
2023-10-21 18:30:14 -0600

    fix: remove pry
    CO-AUTHORED-BY: <kainacockett@gmail.com>

--- app/channels/data_channel.rb
+++ app/channels/data_channel.rb
@@ -9,7 +9,6 @@ class DataChannel < ApplicationCable::Channel
     game = Game.find_by(game_name: game_name)
     puzzle = Puzzle.find_by(puzzle_identifier: puzzle_identifier, room_id: game.room_id)
     room_name = Room.find(game.room_id).room_name
-    # require 'pry'; binding.pry
     game_puzzle = GamePuzzle.find_by(puzzle_id: puzzle.id, game_id: game.id)
     game_status = game_puzzle.update_puzzle(room_name: room_name, puzzle_identifier: puzzle_identifier, game_name: game_name)
     ActionCable.server.broadcast("data_#{params[:game_name]}", data)
